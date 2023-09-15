b56f0a97a59681cac46a66902c64e6212a8aebc6
Islam <islamkamran332@gmail.com>
2023-09-15 12:00:49 +0500

    lecture 1 intermediate completed

--- class#1/objects.py
+++ class#1/objects.py
@@ -1,22 +1,31 @@
 class Person:
+    # class variables
+    amount = 0
+
     def __init__(self, name,age):
         self.name = name
         self.age = age
-    
-    def other_activities(self, chai):
-        self.chai = chai
-        return self.chai
-    
+        # self is for accessing the data inside of the function to access data inside of the class we write the class name in this case to access amount
+        Person.amount += 1
+
     # to delete an object after the work is complete
     def __del__(self):
-        print("Object Deleted")
+        Person.amount -= 1
+
+
+    # def other_activities(self, chai):
+    #     self.chai = chai
+    #     return self.chai
+    
+    def __str__(self):
+        return f'Name: {self.name}, Age:{self.age}'
+    
 
 person1 = Person("islam kamran", 27)
-print(person1.name, person1.age)
-person1.name = "Abdullah"
-print(person1.name, person1.age)
+print(person1)
+person2 = Person("Arsalan", 28)
+print(Person.amount)
+print(person2)
+del person2
+print(Person.amount)
 
-activites_person1 = person1.other_activities("Garma chai")
-print(activites_person1)
-
-del person1
